10421bece070a886696a5134a7e180506f71dc98
jaal5395 <jaal5395@student.su.se>
2025-05-22 11:09:09 +0200

    Add ListEdge

---
+++ backend/src/main/java/se/su/inlupp/ListEdge.java
@@ -0,0 +1,58 @@
+package se.su.inlupp;
+
+import java.util.Objects;
+
+public class ListEdge<T> implements Edge<T>{
+    private final T destination;
+    private final String name;
+    private int weight;
+
+    public ListEdge(T destination, String name, int weight){
+        this.destination = destination;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public int getWeight(){
+        return weight;
+    }
+
+    @Override
+    public void setWeight(int weight){
+        Objects.requireNonNull(weight);
+        if (weight < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.weight = weight;
+        }
+    }
+
+    @Override
+    public T getDestination() {
+        return this.destination;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return "ListEdge[destination=" + destination + ", name=" + name + " weight=" + weight + "]";
+    }
+
+    // @Override
+    // public boolean equals(Object other){
+    //     if(other instanceof ListEdge o){
+    //         return this.destination == o.destination;
+    //     }
+    //     return false;
+    // }
+
+    // @Override
+    // public int hashCode(){
+    //     return Objects.hash(name, destination);
+    // }
+}
